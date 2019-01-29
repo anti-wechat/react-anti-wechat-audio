977a05507769eb3d04f7667438638d9010cdfc69
littlee <usedlittlee@gmail.com>
2019-01-29 10:57:39 +0800

    init project

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const libName = 'react-anti-wechat-audio';
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: libName + '.js',
+    library: libName,
+    libraryExport: 'default',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    react: 'react'
+  }
+};
